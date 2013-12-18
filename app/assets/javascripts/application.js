437fa565e2ac44c409771ebe69fff4ebb5362ae1
Jen Dewalt <jen.dewalt@gmail.com>
2013-12-18 15:03:01 -0800

    Add analytics

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require analytics
 //= require jquery
 //= require jquery_ujs
 //= require underscore
