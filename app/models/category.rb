13778985c1c2196927db1a592e18046cd64a4e01
Jen Dewalt <jen.dewalt@gmail.com>
2014-02-23 20:43:55 -0800

    Return categories in alpha order by default

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ActiveRecord::Base
+  default_scope :order => 'categories.name ASC'
   has_many :categorizations, :dependent => :destroy
   has_many :establishments, :through => :categorizations
 end
