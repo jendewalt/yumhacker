d8cbb22967263738b0767511fe83f81bfaa5a082
Jen Dewalt <jen.dewalt@gmail.com>
2014-04-21 17:16:57 -0700

    Add favorite button to home page lists

--- app/assets/javascripts/views/home_page/list_view.js
+++ app/assets/javascripts/views/home_page/list_view.js
@@ -4,6 +4,15 @@ HomePageListView = Backbone.View.extend({
 
     initialize: function () {
         this.render();
+
+        this.favorite_button_view = new ApplicationFavoriteButtonView({ 
+            el: this.$('.favorite_btn_container'),
+            model: new FavoriteButton({
+                'list_id': this.model.get('id'), 
+                'user_favoriting': this.model.get('user_favoriting'), 
+                'number_favorites': this.model.get('number_favorites') 
+            })
+        });     
     },
 
     render: function () {
