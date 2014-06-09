e0a93bcaba2fc54e59b436c977ce3bd41acaebef
Jen Dewalt <jen.dewalt@gmail.com>
2014-06-09 16:58:36 -0700

    Remove featured user from home page

--- app/assets/javascripts/views/home_page/featured_view.js
+++ app/assets/javascripts/views/home_page/featured_view.js
@@ -5,23 +5,22 @@ HomePageFeaturedView = Backbone.View.extend({
     initialize: function () {
         this.render();
 
-        this.featured_list = new List({ id: 453 });
-        this.featured_user = new User({ id: 284 });
-
-        this.featured_user_view = new HomePageFeaturedUserView({
-            el: '#featured_user',
-            model: this.featured_user
-        });
+        this.featured_list = new List({ id: 486 });
 
         this.featured_list_view = new HomePageFeaturedListView({
             el: '#featured_list',
             model: this.featured_list
         });
+
+        // Removed featured user splash to focus on content. 
+        // this.featured_user = new User({ id: 284 });
+        // this.featured_user_view = new HomePageFeaturedUserView({
+        //     el: '#featured_user',
+        //     model: this.featured_user
+        // });
     },
 
     render: function () {
         this.$el.html(render('home_page/featured'));
-
     }
-
 });
